1cb6e5e925b8976fa9b091ad136dcac0236b662b
Lawlrng <lawlrng@gmail.com>
2014-04-22 20:32:53 -0400

    Project start\!

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    logger = require('morgan'),
+    favicon = require('static-favicon'),
+    http = require('http'),
+    path = require('path'),
+
+    app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
