fd217ed430c398a96c47c9a55a1deb27f6624d6f
Lawlrng <lawlrng@gmail.com>
2014-04-22 23:00:18 -0400

    Added some styles and stuff.

--- public/js/interaction.js
+++ public/js/interaction.js
@@ -1,7 +1,11 @@
 $("img.choice").click(function () {
     var $this = $(this),
         choice = $this.attr("title"),
+        $li = $this.parent(),
         $img = $(document.createElement("img"));
+
+    $li.siblings().removeClass("selected");
+    $li.addClass("selected");
 
     $img.attr("src", "img/full/" + choice + ".png")
         .attr("title", choice).attr("alt", choice);
